eeb8ec64873aedf4d21d90e46f079ec8ebe6bdee
Florian Braun <florian.braun@tonies.com>
2022-12-08 14:16:44 +0100

    preheat gunicorn workers before starting k6 benchmarks to avoid outliers in statistics

--- benchmarks/run-benchmarks.sh
+++ benchmarks/run-benchmarks.sh
@@ -4,6 +4,11 @@ set -Eeuo pipefail
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
+echo "preheating $API_HOST"
+for ((i=1;i<=100;i++)); do
+  curl -s $API_HOST > /dev/null
+done
+
 for bench in "$@"; do
   f="$(basename -- $bench .js)"
   set -x
