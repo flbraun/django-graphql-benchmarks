2d843b9c1db34722d3fad3b188b73446002a0df2
Florian Braun <florian.braun@tonies.com>
2023-01-10 14:17:35 +0100

    make the bench suite runnable with multiple Python versions; upgrade dependencies

--- run.sh
+++ run.sh
@@ -2,21 +2,25 @@
 
 set -e
 
-git checkout main
-docker build -t benchapp .
-
-git checkout main_graphenev2_only
-docker build -t benchapp-only-graphenev2 .
-git checkout main
-
-docker run --detach -p "8097:8000" --name benchapp benchapp
-docker run --detach -p "8098:8000" --name benchapp-only-graphenev2 -e ONLY_GRAPHENE_V2=true benchapp-only-graphenev2
-sleep 5; # wait for gunicorn works to be available
-
 rm -f benchmarks/results/*
-API_HOST="http://localhost:8097" benchmarks/run-benchmarks.sh drf json strawberry graphene
-API_HOST="http://localhost:8098" benchmarks/run-benchmarks.sh graphenev2
-docker rm --force $(docker container ls -q --filter "name=benchapp*")
+
+for pyversion in "$@"
+do
+  git checkout main
+  docker build -t benchapp --build-arg BASE_IMAGE=python:${pyversion}-slim-bullseye .
+
+  git checkout main_graphenev2_only
+  docker build -t benchapp-only-graphenev2 --build-arg BASE_IMAGE=python:${pyversion}-slim-bullseye .
+  git checkout main
+
+  docker run --detach -p "8097:8000" --name benchapp benchapp
+  docker run --detach -p "8098:8000" --name benchapp-only-graphenev2 -e ONLY_GRAPHENE_V2=true benchapp-only-graphenev2
+  sleep 5; # wait for gunicorn works to be available
+
+  API_HOST="http://localhost:8097" benchmarks/run-benchmarks.sh ${pyversion} drf json strawberry graphene
+  API_HOST="http://localhost:8098" benchmarks/run-benchmarks.sh ${pyversion} graphenev2
+  docker rm --force $(docker container ls -q --filter "name=benchapp*")
+done
 
 poetry run python benchmarks/format-results.py
 DASHBOARD_PORT=8099 poetry run python benchmarks/dashboard.py
