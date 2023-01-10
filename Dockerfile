1e79d87ed3ea811443c7f15b26f30c4e586d2d49
Florian Braun <florian.braun@tonies.com>
2023-01-10 14:17:35 +0100

    make the bench suite runnable with multiple Python versions; upgrade dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3.10-slim-bullseye
+ARG BASE_IMAGE
+FROM $BASE_IMAGE
 
 WORKDIR /djapp
 ENV PATH="${PATH}:/root/.local/bin"
--- benchmarks/run-benchmarks.sh
+++ benchmarks/run-benchmarks.sh
@@ -4,14 +4,21 @@ set -Eeuo pipefail
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-echo "preheating $API_HOST"
+pyversion=$1
+shift
+frameworks=$@
+
+echo "preheating endpoints at $API_HOST"
 for ((i=1;i<=100;i++)); do
-  curl -s $API_HOST > /dev/null
+  curl -s $API_HOST/json-api/top-250 > /dev/null
+  curl -s $API_HOST/drf-api/movies/ > /dev/null
+  curl -s -X POST $API_HOST/strawberry-graphql/ -H "Content-Type: application/json" -d '{"query": "{\n __schema {\n types {\n name\n }\n }\n}"}' > /dev/null
+  curl -s -X POST $API_HOST/graphene-graphql/ -H "Content-Type: application/json" -d '{"query": "{\n __schema {\n types {\n name\n }\n }\n}"}' > /dev/null
 done
 
-for bench in "$@"; do
+for bench in $frameworks; do
   f="$(basename -- $bench .js)"
   set -x
-  k6 run --out json="$script_dir/results/$f.json" "$script_dir/$f.js"
+  k6 run --out json="$script_dir/results/$f@py$pyversion.json" "$script_dir/$f.js"
   set +x
 done
